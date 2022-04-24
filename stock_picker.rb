d631cc26db0543fe15cc1db870b04a5aaaa74574
Bob <bobdt84@gmail.com>
2022-04-23 21:49:43 -0500

    Rubocop autocorrect linter used

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,16 +1,16 @@
 def stock_picker(prices)
-    buy_sell = []
-    best_profit = 0
-    prices.each_with_index do |price, index|
-        if index < (prices.length - 1) 
-            max = prices.slice((index + 1)..-1).max #max of array of prices after current index
-            if max - price > best_profit
-                best_profit = max - price
-                buy_sell = [index, prices.index(max)]
-            end
-        end
+  buy_sell = []
+  best_profit = 0
+  prices.each_with_index do |price, index|
+    next unless index < (prices.length - 1)
+
+    max = prices.slice((index + 1)..-1).max # max of array of prices after current index
+    if max - price > best_profit
+      best_profit = max - price
+      buy_sell = [index, prices.index(max)]
     end
-    buy_sell
+  end
+  buy_sell
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10]) #=> [1,4]
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10]) #=> [1,4]
